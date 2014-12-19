6d49c11294f186f7f93b584aa51e0fd732a5eea5
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-19 10:58:25 +0000

    Revert "Changes to CV"
    This reverts commit 0de6b35e57e80bd8a62e796243b5b4c3e45beec9.

--- index.php
+++ index.php
@@ -6,70 +6,21 @@ require_once 'core/init.php'; ?>
 <html>
 <head>
 	<title></title>
-
-      <!-- {{ mustache.js }} placeholders to target different JSON elements -->
-      <script id="characterstpl" type="text/template">      
-      {{#characters}}
-      <div class="character">
-         <img src="images/{{shortname}}_tn.jpg" alt="Photo of {{name}}" />
-         <h3>{{name}}</h3>
-         <h4>{{reknown}}</h4>
-         <p>{{bio}}</p>
-       </div>
-         {{/characters}}
-      </script>
-      <link href="css/styley.min.css" rel="stylesheet" type="text/css"/>
-      <link href="css/styleCar.css" rel="stylesheet" type="text/css"/>
-      <!-- https://cdnjs.com/-->
-      <script src="js/libs/jquery/jquery.js" type="text/javascript"></script>      
-      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.cycle/3.03/jquery.cycle.all.min.js" type="text/javascript"></script>
-      <script src="js/libs/mustache.js/mustache.js" type="text/javascript"></script>
-
-      <script type="text/javascript">
-         $(function () {
-
-            $.getJSON('data.json', function (data) {
-               var template = $('#characterstpl').html();
-               var html = Mustache.to_html(template, data);
-               $('#carousel').html(html);
-               
-               $('#carousel').cycle({
-                  fx: 'fade',
-                  pause: 1,
-                  next: '#next_btn',
-                  prev: '#prev_btn',
-                  speed: 500,
-                  slideResize: true,
-                 containerResize: false,
-                 width: '100%',
-                 fit: 1,
-                  timeout: 10000                  
-               }); // cycle.js 
-            }); // getJSON            
-         }); // function
-      </script>
 </head>
 <body>
-
-<div id="content">
-                <!-- Page #1 -->
-                <article id = "page-projects" class="page-round">
-
-                    <!-- Page Project -->
-                    <div class="pageStyle">
-      
-      <div id="characterbox"> 
-          <a href ="#" id="prev_btn">&laquo;</a>
-          <a href ="#" id="next_btn">&raquo;</a>
-         <a href="https://github.com/Stevo5o/JSON-Carousel">
-            <img id="git" src="images/github.png" alt="github"/></a> 
-         <h2>Projects</h2>                 
-         <div id="carousel"></div>          
-      </div>
-      <br>
-</div>
-</article>
-
+<?php 
+$book = DB::getInstance()->query("SELECT * FROM books");
+if(!$book->count()) {
+	echo 'No book';
+} else {
+	foreach ($book->results() as $book) {
+		echo $book->author_name, '<br>',
+			 $book->title, '<br>',
+			 $book->link, '<br>',
+		     $book->image, '<br>';
+	}
+}
+?>
 
 				<!-- Page #2 -->					
                 <article id="page-contact" class="page-round">
@@ -109,20 +60,5 @@ require_once 'core/init.php'; ?>
                     </form>
                  				
                 </article>
-
-                <?php 
-$book = DB::getInstance()->query("SELECT * FROM books");
-if(!$book->count()) {
-    echo 'No book';
-} else {
-    foreach ($book->results() as $book) {
-        echo $book->author_name, '<br>',
-             $book->title, '<br>',
-             $book->link, '<br>',
-             $book->image, '<br>';
-    }
-}
-?>
-</div>
                 </body>
 </html>
